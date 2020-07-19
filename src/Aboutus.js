ce9e9c2e3e29398a7155cdb7e1e617b9b58ea721
adeshsingh0604 <adeshsingh0604@users.noreply.github.com>
2020-07-19 15:28:00 +0530

    second commit

--- src/Aboutus.js
+++ src/Aboutus.js
@@ -29,9 +29,7 @@ function Aboutus() {
                             <p> DevikTech trains students to develop their skills in Coding. These students in turn participate in different competitions around the country and globe and  the very participation creates a sense of winning among many students. Some students go on to win these competitions and thus bring laurels to the school. Thus the school not only becomes a better place to learn, but is also ahead of other schools who are not implementing something like this.
                             Besides these the quality of skills among teachers also improves and they become more and more involved with technology.
                     </p>
-                            <p>And we have the example right now ,many government schools are struggling to conduct online classes, not because they don’t have resources(they are already provided laptops and video conferencing accounts by authorities, but because they don’t have the skills to operate online.
-
-                    </p>
+                            <p>And we have the example right now ,many government schools are struggling to conduct online classes, not because they don’t have resources(they are already provided laptops and video conferencing accounts by authorities, but because they don’t have the skills to operate online.</p>
                             <button className="applybutton">Book A Demo</button>
                         </div>
 
@@ -46,9 +44,10 @@ function Aboutus() {
                             <div className="founderpara">
                                 <p>DevikTech is founded by <a href="https://www.devikTech.com">Vikash Dubey</a>
                                 , best teacher and amazing personality.Vikash is a Bachelor of Technology in Computer Science from Sharda University, Greater Noida, NCR, Uttar Pradesh. He has been interested to imparting knowledge to everyone around him since quite young age. It started with teaching his own brother in school, then it expanded to the tuition of neighbouring kids for helping them completing their homework.  It took a serious turn when he started taking classes for high school students in NOIDA during his Engineering Days. Finally after graduation he worked in an International Edutech firm. He found certain gaps in the knowledge being imparted to students and the skills required for students. To bridge that gap he started his own venture DEVIKTECH EDUCATION LLP which got incorporated in JUNE 2020.</p>
-                                <p>He has a simple philosophy “ getting the students ready for contributing in the process of making INDIA a global engineering power. We train students in coding in various programming languages so that they become more and more involved in the technologies being developed and thus get ready for the technologies of future. We conduct tests for different students based on their class and try to mentor and the best performers to participate in national and international coding competitions.</p>
-                                <button className="founderbutton">Know More</button>
+                                <p>He has a simple philosophy “ getting the students ready for contributing in the process of making INDIA a global engineering power. We train students in coding in various programming languages so that they become more and more involved in the technologies being developed and thus get ready for the technologies of future. We conduct tests for different students based on their class and try to mentor and the best performers to participate in national and international coding competitions.</p>    
                             </div>
+                            <button className="founderbutton">Know More</button>
+                            
 
                         </div>
                         <div className="aboutusfounderimage"><img alt="DevkiTech" src={founderimage} width="450px" height="450px" />
--- src/mainfooter.js
+++ src/mainfooter.js
@@ -71,9 +71,9 @@ function Footer() {
 
 
                <div className="footersocialdiv">
-                    <div className="socialseactiondiv">
-                         <h3><p>Copyright © 2020 All rights reserved | <a className="footer-link" href="index.html">DevikTech</a></p></h3>
-                    </div>
+                    <p>Copyright © 2020 DevikTech Company India. All rights reserved. | <a className="footer-link" href="index.html">DevikTech</a></p>
+                    <p> <a className="footer-link" href="index.html">Teams and conditions</a></p>
+                    <p><a className="footer-link" href="index.html">Privacy policy</a></p>         
                     <div className="copyrightdiv">
                          <div className="socialmedia">
                               <a href="index.html"><img alt="DevkiTech" src={require("C:/Users/Hp/first-app-sample/src/images/facebook.png")} width="40px" height="40px" /></a>
@@ -88,7 +88,127 @@ function Footer() {
                          </div>
                     </div>
                </div>
-          </div>
+
+
+
+
+               {/* <footer id="footer" class="body-footer">
+                    <div className="container">
+                         <div class="row mg-none mg-bottom-lv4-i row--vertical-center footer__top">
+                              <a href="#index"><img src="https://media.flaticon.com/dist/min/img/logo/flaticon_white.svg" alt="Flaticon" width="154"/></a>
+
+                                   <div class="push-right footer__buttons">
+                                        <div class="row mg-none">
+                                             <a href="https://contributor.flaticon.com/?utm_source=menu&amp;utm_medium=menu_en&amp;utm_campaign=earn_money_en2" class="bj-button bj-button--outline bj-button--green" target="_blank" rel="noreferrer noopener">Become a contributor</a>
+                                             <a href="https://www.flaticon.com/profile/premium?lang=en" class="bj-button bj-button--flat bj-button--yellow mg-left-lv2 fi-not-premium track" data-track-arguments="ga, event, premium, footer">Go Premium</a>
+                                        </div>
+                                   </div>
+		               </div>
+
+                              <div class="row mg-none">
+
+                                   <div class="footer__column">
+                                        <p class="footer__title"><label for="footer__content">Content<i class="bj-icon bj-icon--sm bj-icon--next floatr show-phone"></i></label></p>
+                                        <input type="checkbox" id="footer__content"/>
+                                             <ul>
+                                                  <li><a href="https://www.flaticon.com/most-downloaded">Top icons</a></li>
+                                                  <li><a href="https://www.flaticon.com/latest-icons" id="link-menu-latest-icons">New icons</a></li>
+                                                  <li><a href="https://www.flaticon.com/packs">New packs</a></li>
+                                                  <li><a href="https://www.flaticon.com/tags">Tags</a></li>
+                                                  <li><a href="https://www.flaticon.com/categories" id="link-menu-categories">Categories</a></li>
+                                                  <li><a href="https://www.flaticon.com/authors">Authors</a></li>
+                                             </ul>
+			                    </div>
+
+                                        <div class="footer__column">
+                                             <p class="footer__title"><label for="footer__apps">APPs<i class="bj-icon bj-icon--sm bj-icon--next floatr show-phone"></i></label></p>
+                                             <input type="checkbox" id="footer__apps"/>
+                                                  <ul>
+                                                       <li><a href="https://developer.flaticon.com/">API</a></li>
+                                                       <li><a href="https://www.flaticon.com/font-face">@font-face generator</a></li>
+                                                       <li class="hide-mobile"><a href="https://www.flaticon.com/pattern">Patterns generator</a></li>
+                                                       <li><a href="https://www.flaticon.com/for-google">Google Suite</a></li>
+                                                  </ul>
+			               </div>
+
+                                             <div class="footer__column">
+                                                  <p class="footer__title"><label for="footer__help">Help<i class="bj-icon bj-icon--sm bj-icon--next floatr show-phone"></i></label></p>
+                                                  <input type="checkbox" id="footer__help"/>
+                                                       <ul>
+                                                            <li><a href="https://www.flaticon.com/profile/support">Support</a></li>
+                                                            <li><a href="https://support.flaticon.com/hc/en-us">FAQs</a></li>
+                                                            <li><a href="https://www.flaticon.com/styles">Icon styles</a></li>
+                                                            <li><a href="https://www.flaticon.com/icons-editor">Icons editor</a></li>
+                                                            <li><a href="https://www.flaticon.com/collections">Flaticon Collections</a></li>
+                                                            <li><a href="https://www.flaticon.com/pattern-generator">Patterns generator</a></li>
+                                                            <li><a href="https://www.flaticon.com/follow">Follow</a></li>
+                                                       </ul>
+			     </div>
+
+                                                  <div class="footer__column">
+                                                       <p class="footer__title"><label for="footer__company">Company<i class="bj-icon bj-icon--sm bj-icon--next floatr show-phone"></i></label></p>
+                                                       <input type="checkbox" id="footer__company"/>
+                                                            <ul>
+                                                                 <li><a href="https://www.freepikcompany.com/about_us" target="_blank" rel="noreferrer noopener">About</a></li>
+                                                                 <li><a href="https://www.flaticon.com/profile/support">Contact us</a></li>
+                                                                 <li><a href="https://www.freepikcompany.com/legal#nav-flaticon-agreement" target="_blank" rel="noreferrer noopener">Our license</a></li>
+                                                                 <li><a href="https://www.flaticon.com/affiliates">Affiliation</a></li>
+                                                                 <li><a href="https://www.freepik.com/blog?__hstc=57440181.2e40f478c9aff8002b82e2835e35dd5f.1595070132812.1595070132812.1595141671183.2&amp;__hssc=57440181.1.1595141671183&amp;__hsfp=3041347479" target="_blank" rel="noreferrer noopener">Blog</a></li>
+                                                                 <li><a href="https://www.flaticon.com/profile/premium?lang=en" target="_blank" rel="noreferrer noopener">Plans and Pricing</a></li>
+                                                            </ul>
+			                                   </div>
+
+                                                       <div class="footer__column footer__social">
+                                                            <p class="footer__title"><label for="footer__social">Social media</label></p>
+                                                            <ul class="share">
+                                                                 <li><a href="https://www.facebook.com/theflaticon" class="flaticon-facebook bg-facebook" title="Facebook" target="_blank" rel="noreferrer noopener"></a></li>
+                                                                 <li><a href="https://twitter.com/theflaticon" class="flaticon-twitter bg-twitter" title="Twitter" target="_blank" rel="noreferrer noopener"></a></li>
+                                                                 <li><a href="https://www.pinterest.com/flaticon" class="flaticon-pinterest bg-pinterest" title="Pinterest" target="_blank" rel="noreferrer noopener"></a></li>
+                                                                 <li><a href="https://www.instagram.com/freepik" class="flaticon-instagram bg-instagram" title="Instagram" target="_blank" rel="noreferrer noopener"></a></li>
+                                                                 <li><a href="https://www.youtube.com/channel/UC9wPXN2TIWoUamqONb7v8Pw" class="flaticon-youtube bg-youtube" title="YouTube" target="_blank" rel="noreferrer noopener"></a></li>
+                                                            </ul>
+
+                                                            <div class="gr_not-connected">
+                                                                 <p class="bold">Get exclusive resources straight to your inbox</p>
+                                                                 <a href="https://www.flaticon.com/register" class="bj-button bj-button--outline bj-button--green bj-button--sm track" data-track-arguments="ga, event, register, click, footer-register">Sign up</a>
+                                                            </div>
+                                                       </div>
+
+                                                  </div>
+
+                                                  <ul class="footer__legal font-xs mg-top-lv3">
+                                                       <li class="inline-block"><a href="https://www.freepikcompany.com/legal#nav-flaticon" target="_blank" rel="noreferrer noopener" title="Terms &amp; conditions">Terms &amp; conditions</a></li>
+                                                       <li class="inline-block mg-left-lv2"><a href="https://www.freepikcompany.com/privacy" target="_blank" rel="noreferrer noopener" title="Privacy policy">Privacy policy</a></li>
+                                                       <li class="inline-block mg-left-lv2"><a href="https://www.freepikcompany.com/cookie" target="_blank" rel="noreferrer noopener" title="Cookies policy">Cookies policy</a></li>
+                                                       <li class="inline-block mg-left-lv2"><a href="https://www.freepikcompany.com/copyright" target="_blank" rel="noreferrer noopener" title="Copyrights Notification">Copyrights Notification</a></li>
+                                                  </ul>
+
+                                                  <div class="copyright">
+                                                       <div class="row mg-none">
+                                                            <div class="col mg-none pd-none alignl pd-right-lv2-i">
+                                                                 <div class="row row--vertical-center mg-none">
+                                                                      <a href="https://www.freepikcompany.com" target="_blank" rel="noreferrer noopener"><img src="https://media.flaticon.com/dist/min/img/logo/freepikcompany.svg" alt="Freepik Company" class="mg-right-lv1"/></a>
+                                                                           <p class="mg-none">Copyright © 2010-2020 Freepik Company S.L. All rights reserved.</p>
+					                                        </div>
+                                                            </div>
+                                                       </div>
+                                                  </div>
+
+                                                  </div>
+
+                                                  <div class="footer__projects">
+                                                       <div class="container">
+                                                            <span>Freepik company projects</span>
+                                                            <ul>
+                                                                 <li><a href="https://www.freepik.com/?__hstc=57440181.2e40f478c9aff8002b82e2835e35dd5f.1595070132812.1595070132812.1595141671183.2&amp;__hssc=57440181.1.1595141671183&amp;__hsfp=3041347479" target="_blank" rel="noreferrer noopener">Freepik</a></li>
+                                                                 <li><a href="/">Flaticon</a></li>
+                                                                 <li><a href="http://www.piktab.com" target="_blank" rel="noreferrer noopener">Piktab</a></li>
+                                                                 <li><a href="https://www.slidesgo.com" target="_blank" rel="noreferrer noopener">Slidesgo</a></li>
+                                                            </ul>
+                                                       </div>
+                                                  </div>
+</footer> */}
+                                        </div>
      )
 }
 
